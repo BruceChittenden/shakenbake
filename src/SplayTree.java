45023cfeffdb67187253287cfe5926a46b3925f3
cse326j <cse326j@5e1dc367-633f-0410-a614-1108982342f5>
2007-11-28 02:49:27 +0000

    Code complete with AVL and Splay trees--the code is there and they seem to work with some minimal testing. More testing is required to be sure that these classes work. Also, comments and code cleanup are needed.

--- src/SplayTree.java
+++ src/SplayTree.java
@@ -27,15 +27,20 @@ public class SplayTree implements StringCounter
 	// IncCount increments the count for a particular string
 	public void IncCount(String s)
 	{
-		insert(s, root);
+		root = insert(s, root);
+		counter = 0;
 	}
-
-	// recursive helper for IncCount -- NOT DONE! DOESN'T SPLAY
+	
+	// counter to help with the splaying
+	private static int counter = 0;
+	
+	// recursive helper for IncCount
 	public Node insert(String s, Node root)
 	{
 		if(root == null)
 		{
 			root = new Node(s);
+			counter++;
 			return root;
 		}
 		
@@ -44,14 +49,139 @@ public class SplayTree implements StringCounter
 		if(compare < 0)
 		{
 			root.left = insert(s, root.left);
+			
+			// check if we need to do a rotation
+			if(counter == 2)
+			{
+				// do a zig-zig or zig-zag rotation
+				counter = 0;
+				
+				if(s.compareTo(root.left.str) < 0)
+				{
+					// do a zig-zig
+					Node top, left, leftLeft;
+					
+					top = root;
+					left = root.left;
+					leftLeft = left.left;
+					
+					top.left = left.right;
+					
+					left.left = leftLeft.right;
+					left.right = top;
+					
+					leftLeft.right = left;
+					
+					root = leftLeft;
+				}
+				else
+				{
+					// do a zig-zag
+					Node top, left, leftRight;
+					
+					top = root;
+					left = root.left;
+					leftRight = left.right;
+					
+					top.left = leftRight.right;
+					
+					left.right = leftRight.left;
+					
+					leftRight.left = left;
+					leftRight.right = top;
+					
+					root = leftRight;
+				}
+				
+			}
+			else if(root == this.root)
+			{
+				// do a zig rotation
+				Node top, left;
+				
+				top = root;
+				left = root.left;
+				
+				top.left = left.right;
+				
+				left.right = top;
+				
+				root = left;
+			}
+			
+			counter++;
+			return root;
 		}
 		else if(compare > 0)
 		{
 			root.right = insert(s, root.right);
+			
+			// check if we need to do a rotation
+			if(counter == 2)
+			{
+				// do a zig-zig or zig-zag rotation
+				counter = 0;
+				
+				if(s.compareTo(root.right.str) > 0)
+				{
+					// do a zig-zig
+					Node top, right, rightRight;
+					
+					top = root;
+					right = root.right;
+					rightRight = right.right;
+					
+					top.right = right.left;
+					
+					right.right = rightRight.left;
+					right.left = top;
+					
+					rightRight.left = right;
+					
+					root = rightRight;
+				}
+				else
+				{
+					// do a zig-zag
+					Node top, right, rightLeft;
+					
+					top = root;
+					right = root.right;
+					rightLeft = right.left;
+					
+					top.right = rightLeft.left;
+					
+					right.left = rightLeft.right;
+					
+					rightLeft.left = right;
+					rightLeft.right = top;
+					
+					root = rightLeft;
+				}
+				
+			}
+			else if(root == this.root)
+			{
+				// do a zig rotation
+				Node top, right;
+				
+				top = root;
+				right = root.right;
+				
+				top.right = right.left;
+				
+				right.left = top;
+				
+				root = right;
+			}
+			
+			counter++;
+			return root;
 		}
 		else
 		{
 			root.cnt++;
+			counter++;
 			return root;
 		}
 	}
@@ -87,5 +217,60 @@ public class SplayTree implements StringCounter
 		index = GetCounts(root.right, counts, index);
 		
 		return index;
+	}
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Splay Tree Testing");
+		
+		HashTable table = new HashTable();
+		
+		table.IncCount("hello");
+		table.IncCount("hello");
+		
+		table.IncCount("world");
+		table.IncCount("world");
+		table.IncCount("world");
+		table.IncCount("world");
+		
+		table.IncCount("Happy Thanksgiving!");
+		table.IncCount("Happy Thanksgiving!");
+		table.IncCount("Happy Thanksgiving!");
+		
+		table.IncCount("Food");
+		table.IncCount("Food");
+		table.IncCount("Food");
+		table.IncCount("Food");
+		table.IncCount("Food");
+		table.IncCount("Food");
+		table.IncCount("Food");
+		
+		table.IncCount("cool");
+		
+		table.IncCount("Assignment due");
+		table.IncCount("Assignment due");
+		
+		table.IncCount("Wednesday");
+		
+		table.IncCount("night");
+		table.IncCount("night");
+		
+		table.IncCount("at");
+		
+		table.IncCount("TWELVE!!!");
+		table.IncCount("TWELVE!!!");
+		table.IncCount("TWELVE!!!");
+		table.IncCount("TWELVE!!!");
+		table.IncCount("TWELVE!!!");
+		
+		StringCount[] counts = table.GetCounts();
+		
+		for(int i = 0; i<counts.length; i++)
+		{
+			if(counts[i] != null)
+				System.out.print("[" + counts[i].str +"," + counts[i].cnt + "], ");
+			else
+				System.out.print("NULL!!!!! " + i);
+		}		
 	}
 }
